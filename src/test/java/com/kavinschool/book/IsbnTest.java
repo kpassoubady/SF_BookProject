cf32cd4ff4ce3932a2bf8296d8d4fd84fa870d67
Kangs <>
2020-05-05 08:52:45 -0700

    changed test case

--- src/test/java/com/kavinschool/book/IsbnTest.java
+++ src/test/java/com/kavinschool/book/IsbnTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class IsbnTest {
 
 	@Test
-	public void checkAValid10DigitISBN() {
+	public void checkAValid10DigitIsbn() {
 		Isbn validator = new Isbn();
 		boolean result = validator.isIsbn("0140449116");
 		assertTrue("first value", result);
@@ -16,7 +16,7 @@ public class IsbnTest {
 	}
 	
 	@Test 
-	public void checkAValid13DigitISBN() {
+	public void checkAValid13DigitIsbn() {
 		Isbn validator = new Isbn();
 		boolean result = validator.isIsbn("9781853260087");
 		assertTrue("first value", result);
@@ -25,34 +25,34 @@ public class IsbnTest {
 	}
 	
 	@Test
-	public void TenDigitISBNNumbersEndingInAnXAreValid() {
+	public void TenDigitIsbnNumbersEndingInAnXAreValid() {
 		Isbn validator = new Isbn();
 		boolean result = validator.isIsbn("012000030X");
 		assertTrue(result);
 	}
 	
 	@Test
-	public void checkAnInvalid10DigitISBN() {
+	public void checkAnInvalid10DigitIsbn() {
 		Isbn validator = new Isbn();
 		boolean result = validator.isIsbn("0140449117");
 		assertFalse(result);
 	}
 	
 	@Test
-	public void checkAnInvalid13DigitISBN() {
+	public void checkAnInvalid13DigitIsbn() {
 		Isbn validator = new Isbn();
 		boolean result = validator.isIsbn("9781853267336");
 		assertFalse(result);
 	}
 	
 	@Test(expected = NumberFormatException.class)
-	public void nineDigitISBNsAreNotAllowed() {
+	public void nineDigitIsbnsAreNotAllowed() {
 		Isbn validator = new Isbn();
 		validator.isIsbn("123456789");
 	}
 	
 	@Test(expected = NumberFormatException.class)
-	public void nonNumericISBNsAreNotAllowed() {
+	public void nonNumericIsbnsAreNotAllowed() {
 		Isbn validator = new Isbn();
 		validator.isIsbn("ThisIsKangs");
 	}
